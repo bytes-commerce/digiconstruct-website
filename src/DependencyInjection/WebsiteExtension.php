068a6fbeff165497820ba255ec2a19ffcb3d7243
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2024-12-13 15:55:36 +0100

    Go back to yaml loader.

--- src/DependencyInjection/WebsiteExtension.php
+++ src/DependencyInjection/WebsiteExtension.php
@@ -5,13 +5,17 @@ namespace DigiConstruct\WebsiteBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class WebsiteExtension extends Extension
+final class WebsiteExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.php');
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
     }
 }
