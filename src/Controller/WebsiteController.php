6a0da6f054e33761e3b0f9efe513310c08aad787
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2024-12-13 14:25:29 +0100

    Adapt Namespaces.

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\WebsiteBundle\Controller;
+namespace DigiConstruct\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
