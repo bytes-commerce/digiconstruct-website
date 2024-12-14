82827382a85f6017b4191f906ac75d6efd05c731
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2024-12-14 02:02:41 +0100

    Added template files, routes, and dummy templates.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,22 +7,90 @@ namespace DigiConstruct\WebsiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class DefaultController extends AbstractController
 {
+    public function __construct(
+        private readonly TranslatorInterface $translator
+    ) {}
+
     #[Route('/', name: 'web.app.index')]
     public function hello(): Response
     {
-        return $this->render('@Website/hello.html.twig', [
-            'message' => 'Hello from the Website Bundle!'
+        return $this->render('@Website/index.html.twig');
+    }
+
+    #[Route(path: [
+        'en' => '/en/about-digiconstruct-software-solutions-for-construction-companies',
+        'de' => '/de/uber-digiconstruct-digitale-loesung-fuer-bauunternehmen'
+    ], name: 'web.app.about')]
+    public function aboutAction(): Response
+    {
+        return $this->render('@Website/pages/about-digiconstruct.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.about.title')
         ]);
     }
 
-    #[Route('/test-route', name: 'web.app.test')]
-    public function testAction(): Response
+    #[Route(path: [
+        'en' => '/en/pricing',
+        'de' => '/de/kostenuebersicht'
+    ], name: 'web.app.pricing')]
+    public function pricingAction(): Response
     {
-        return $this->render('@Website/php-test.html.twig', [
-            'message' => 'Hello from the Website Bundle!'
+        return $this->render('@Website/pages/pricing.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.pricing.title')
+        ]);
+    }
+
+    #[Route(path: [
+        'en' => '/en/impress',
+        'de' => '/de/impressum'
+    ], name: 'web.app.impress')]
+    public function impressAction(): Response
+    {
+        return $this->render('@Website/pages/impress.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.pricing.title')
+        ]);
+    }
+
+    #[Route(path: [
+        'en' => '/en/contact',
+        'de' => '/de/kontakt'
+    ], name: 'web.app.contact')]
+    public function contactAction(): Response
+    {
+        return $this->render('@Website/pages/contact.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.pricing.title')
+        ]);
+    }
+
+    #[Route(path: [
+        'en' => '/en/data-privacy',
+        'de' => '/de/datenschutzerklaerung'
+    ], name: 'web.app.data_privacy')]
+    public function dataPrivacyAction(): Response
+    {
+        return $this->render('@Website/pages/dataPrivacy.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.dataPrivacy.title')
+        ]);
+    }
+
+    #[Route('/sitemap.xml', name: 'web.sitemap', methods: ['GET'], format: 'xml')]
+    public function sitemapXml(): Response
+    {
+        return $this->render('@Website/sitemap.xml.twig', [
+            'urls' => [
+                'web.app.index',
+                'web.app.about',
+                'web.app.pricing',
+                'web.app.impress',
+                'web.app.contact',
+                'web.app.data_privacy',
+                'web.app.siteDocumentation',
+                'web.app.requestHandeling',
+                'web.app.taskManagement',
+            ],
         ]);
     }
 }
---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigiConstruct\WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final class LandingPageController extends AbstractController
+{
+    public function __construct(
+        private readonly TranslatorInterface $translator
+    ) {}
+
+    #[Route(path: [
+        'en' => '/en/landing/construction-site-documentation',
+        'de' => '/de/landing/digitale-baustellendokumentation'
+    ], name: 'web.app.siteDocumentation')]
+    public function siteDocumentationAction(): Response
+    {
+        return $this->render('@Website/pages/lading/site-documentation.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.siteDocumentation.title')
+        ]);
+    }
+
+    #[Route(path: [
+        'en' => '/en/landing/request-handling',
+        'de' => '/de/landing/anfrage-management'
+    ], name: 'web.app.requestHandeling')]
+    public function requestHandlingAction(): Response
+    {
+        return $this->render('@Website/pages/lading/request-handeling.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.requestHandeling.title')
+        ]);
+    }
+
+    #[Route(path: [
+        'en' => '/en/landing/task-management-system',
+        'de' => '/de/landing/digitale-aufgabenverteilung-online-digiconstruct'
+    ], name: 'web.app.taskManagement')]
+    public function taskManagemenetAction(): Response
+    {
+        return $this->render('@Website/pages/lading/task-management.html.twig', [
+            'page_title' => $this->translator->trans('web.digiconstruct.taskManagement.title')
+        ]);
+    }
+}
