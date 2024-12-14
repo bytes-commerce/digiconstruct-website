ec60c5df2b9021180ad32c4e48d1a6c3597f2690
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2024-12-14 23:38:13 +0100

    Add header component and add example translations.

---
+++ src/Resources/translations/websiteBundle+intl-icu.de.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'web' => [
+        'digiconstruct' => [
+            'about' => [
+                'title' => 'Über DigiConstruct'
+            ],
+            'pricing' => [
+                'title' => 'Preise'
+            ],
+            'dataPrivacy' => [
+                'title' => 'Datenschutz'
+            ],
+            'siteDocumentation' => [
+                'title' => 'Digitale Baustellendokumentation'
+            ],
+            'requestHandeling' => [
+                'title' => 'Anfrage Management'
+            ],
+            'taskManagement' => [
+                'title' => 'Digitale Aufgabenverteilung'
+            ],
+            'timeTracking' => [
+                'title' => 'Digitale Zeiterfassung'
+            ],
+            'contact' => [
+                'title' => 'Kontakt'
+            ],
+            'impress' => [
+                'title' => 'Impressum'
+            ],
+            'features' => [
+                'title' => 'Funktionen'
+            ],
+            'faq' => [
+                'title' => 'FAQ'
+            ],
+            'home' => [
+                'title' => 'Home'
+            ],
+            'login' => [
+                'title' => 'Login'
+            ],
+        ]
+    ]
+];
