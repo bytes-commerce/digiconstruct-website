975efced684113a10d87dc3bcfbb4886b4e97d2b
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2024-12-15 16:13:22 +0100

    Add translations.

--- translations/websiteBundle+intl-icu.de.php
+++ translations/websiteBundle+intl-icu.de.php
@@ -31,7 +31,47 @@ return [
                 'title' => 'Impressum'
             ],
             'features' => [
-                'title' => 'Funktionen'
+                'title' => 'Funktionen',
+                'features1' => [
+                    'title' => 'Zeiterfassung',
+                    'description' => 'Schluss mit Excel-Listen. Erfassen Sie Ihre Zeiten direkt online.'
+                ],
+                'features2' => [
+                    'title' => 'Standortunabhängig',
+                    'description' => 'Egal ob im Büro oder auf der Baustelle. DigiConstruct ist immer dabei.'
+                ],
+                'features3' => [
+                    'title' => 'Digitale Baustellendokumentation',
+                    'description' => 'Dokumentieren Sie Ihre Baustellen digital und haben Sie alle Informationen immer griffbereit.'
+                ],
+                'features4' => [
+                    'title' => 'Digitale Aufgabenverteilung',
+                    'description' => 'Verteilen Sie Aufgaben digital mit Fälligkeiten und behalten Sie den Überblick.'
+                ],
+                'features5' => [
+                    'title' => 'Anfrage Management',
+                    'description' => 'Verwalten Sie Anfragen digital und Automatisiert - mit KI-Technologie.'
+                ],
+                'features6' => [
+                    'title' => 'Kostenlose Testphase',
+                    'description' => 'Testen Sie DigiConstruct 30 Tage kostenlos und unverbindlich.'
+                ],
+                'feature7' => [
+                    'title' => 'Fahrzeugmanagement',
+                    'description' => 'Verwalten und dokumentieren Sie Ihre Fahrzeuge direkt in DigiConstruct.'
+                ],
+                'feature8' => [
+                    'title' => 'Kontaktverwaltung',
+                    'description' => 'Verwalten Sie Ihre Kontakte und haben Sie alle Informationen immer griffbereit.'
+                ],
+                'feature9' => [
+                    'title' => 'Reparaturmanagement',
+                    'description' => 'Auf einen Blick sehen, welche Fahrzeuge repariert werden, oder zum Service müssen.'
+                ],
+                'feature10' => [
+                    'title' => 'Digitale Baustellenkarte',
+                    'description' => 'Behalten Sie den Überblick über Ihre Baustellen und haben Sie alle Informationen immer griffbereit.'
+                ],
             ],
             'faq' => [
                 'title' => 'FAQ'
@@ -44,7 +84,7 @@ return [
             ],
             'register' => [
                 'title' => 'Registrieren'
-            ]
+            ],
         ]
     ]
 ];
